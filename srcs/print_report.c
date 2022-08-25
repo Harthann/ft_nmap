ad3aca76493dfbb08becf84eba44d9fa5439f3f6
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-08-25 17:44:08 +0200

    print report do stats and not show principal

--- srcs/print_report.c
+++ srcs/print_report.c
@@ -1,13 +1,11 @@
 #include "ft_nmap.h"
 #include "args.h"
 
-int		should_print(uint8_t flags, int filtered, int open)
+int		should_print(uint8_t flags, int max_flag)
 {
 	if (verbose)
 		return 1;
-	if ((flags & SET_ACCESS) && (flags & OPEN) && open < filtered)
-		return 1;
-	if ((flags & SET_FILTER) && (flags & FILTERED) && filtered < open)
+	if (flags != max_flag)
 		return 1;
 	return 0;
 }
@@ -15,24 +13,39 @@ int		should_print(uint8_t flags, int filtered, int open)
 void	print_report(t_port_status *ports, uint32_t nb_ports, char *target, char *target_ip)
 {
 	struct servent* servi;
-	int				filtered = 0;
-	int				open = 0;
+	int				flags[16] = {0};
+	int				max_flag;
+	int				max_value;
 
-	for (uint32_t i = 0; i < nb_ports; i++) {
-		if (ports[i].flags & SET_FILTER && ports[i].flags & FILTERED)
-			filtered += 1;
-		else if (ports[i].flags & SET_ACCESS && ports[i].flags & OPEN)
-			open += 1;
+	for (uint32_t i = 0; i < nb_ports; i++)
+		flags[ports[i].flags]++;
+	max_flag = 0;
+	max_value = -1;
+	for (int i = 0; i < 16; i++)
+	{
+		if (flags[i] > max_value)
+		{
+			max_flag = i;
+			max_value = flags[i];
+		}
 	}
 
 	printf("%s scan report for %s (%s)\n", prog_name, target, target_ip);
-	printf("%d filtered, %d open on %d ports.\n", filtered, open, nb_ports);
+	printf("Not shown: %d ", max_value);
+	if (max_flag & SET_ACCESS) {
+		printf("%s", max_flag & OPEN ? "opened" : "closed");
+	}
+
+	if (max_flag & SET_FILTER) {
+		if (max_flag & SET_ACCESS)
+			printf("|");
+		printf("%s", max_flag & FILTERED ? "filtered" : "unfiltered");
+	}
+	printf(" ports.\n");
 	printf("PORT      STATUS            SERVICE\n");
-
-
 	for (uint32_t i = 0; i < nb_ports; i++)
 	{
-		if (should_print(ports[i].flags, filtered, open)) {//(ports[i].flags & SET_ACCESS || ports[i].flags & SET_FILTER) {
+		if (should_print(ports[i].flags, max_flag)) {//(ports[i].flags & SET_ACCESS || ports[i].flags & SET_FILTER) {
 			int		n;
 			int		m = 0;
 
