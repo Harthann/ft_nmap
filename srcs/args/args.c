bbede3b8a6b33a8a90db1b3e8bd51c4bd74c1c54
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-09-02 15:46:42 +0200

    nb ports, nb threads, scan types

--- srcs/args/args.c
+++ srcs/args/args.c
@@ -115,14 +115,24 @@ int			parse_arg(int ac, char **av, scanconf_t *config) {
 			config->portrange[i] = i + 1;
 		config->nb_ports = 1024;
 	}
-	printf("Port range: %d %d\n", config->portrange[0], config->portrange[1]);
-	if (config->nb_threads > config->nb_ports)
-		config->nb_threads = config->nb_ports;
-
-	printf("Threads nummber: %d\n", config->nb_threads);
+	printf("Ports: %d\n", config->nb_ports);
+	printf("Threads: %d\n", config->nb_threads);
 	if (verbose == 0 || verbose == 1)
 		verbose |= SCAN_SYN | SCAN_NULL | SCAN_FIN | SCAN_XMAS | SCAN_ACK | SCAN_UDP;
-	printf("Asked scan: %d\n", verbose);
+	printf("Scan(s) Type(s):");
+	if (verbose & SCAN_SYN)
+		printf(" SYN");
+	if (verbose & SCAN_NULL)
+		printf(" NULL");
+	if (verbose & SCAN_FIN)
+		printf(" FIN");
+	if (verbose & SCAN_XMAS)
+		printf(" XMAS");
+	if (verbose & SCAN_ACK)
+		printf(" ACK");
+	if (verbose & SCAN_UDP)
+		printf(" UDP");
+	printf("\n");
 	config->targets = appendlist(config->targets, g_arglist);
 	return 0;
 }
